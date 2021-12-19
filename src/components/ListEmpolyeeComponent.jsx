7dacfc0168535155a0e909742e685d9edc697db6
jaikrai <jaikrai0@gmail.com>
2021-12-19 18:41:12 -0500

    Ad so, it begains

---
+++ src/components/ListEmpolyeeComponent.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ListEmpolyeeComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div></div>;
+  }
+}
+
+export default ListEmpolyeeComponent;
